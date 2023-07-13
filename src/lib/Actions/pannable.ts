1f2440c113799fa052c8ec1fe91548db7e8a6c58
Chazy <luismanuelchazy@gmail.com>
2023-07-13 22:41:05 +0200

    fix type zoomable panneable, add throttle to thats actions

--- src/lib/Actions/pannable.ts
+++ src/lib/Actions/pannable.ts
@@ -1,3 +1,5 @@
+import { throttle } from '$lib/CGraph'
+
 type Coord = {
   x: number
   y: number
@@ -9,9 +11,19 @@ export type PannableParams = {
   onEnd?: (event: MouseEvent | TouchEvent, coords: Coord) => void
 }
 
-export default function (node: HTMLElement, params?: PannableParams): { destroy?: () => void } {
+export default function (node: HTMLElement | SVGElement, params?: PannableParams) {
   let x: number
   let y: number
+
+  const trotMove = throttle((event: MouseEvent | TouchEvent) => {
+    if (!params || !params.onMove) return
+    const e = event instanceof MouseEvent ? event : event.touches[0]
+    const dx = e.clientX - x
+    const dy = e.clientY - y
+    x = e.clientX
+    y = e.clientY
+    params.onMove(event, { x, y, dx, dy })
+  }, 50)
 
   function handleMousedown(event: MouseEvent | TouchEvent) {
     event.stopPropagation()
@@ -23,22 +35,10 @@ export default function (node: HTMLElement, params?: PannableParams): { destroy?
       params.onStart(event, { x, y })
     }
 
-    window.addEventListener('mousemove', handleMousemove)
+    window.addEventListener('mousemove', trotMove)
     window.addEventListener('mouseup', handleMouseup)
-    window.addEventListener('touchmove', handleMousemove)
+    window.addEventListener('touchmove', trotMove)
     window.addEventListener('touchend', handleMouseup)
-  }
-
-  function handleMousemove(event: MouseEvent | TouchEvent) {
-    const e = event instanceof MouseEvent ? event : event.touches[0]
-    const dx = e.clientX - x
-    const dy = e.clientY - y
-    x = e.clientX
-    y = e.clientY
-
-    if (params && params.onMove) {
-      params.onMove(event, { x, y, dx, dy })
-    }
   }
 
   function handleMouseup(event: MouseEvent | TouchEvent) {
@@ -50,18 +50,22 @@ export default function (node: HTMLElement, params?: PannableParams): { destroy?
       params.onEnd(event, { x, y })
     }
 
-    window.removeEventListener('mousemove', handleMousemove)
+    window.removeEventListener('mousemove', trotMove)
     window.removeEventListener('mouseup', handleMouseup)
-    window.removeEventListener('touchmove', handleMousemove)
+    window.removeEventListener('touchmove', trotMove)
     window.removeEventListener('touchend', handleMouseup)
   }
 
+  // @ts-ignore
   node.addEventListener('mousedown', handleMousedown)
+  // @ts-ignore
   node.addEventListener('touchstart', handleMousedown)
 
   return {
     destroy() {
+      // @ts-ignore
       node.removeEventListener('mousedown', handleMousedown)
+      // @ts-ignore
       node.removeEventListener('touchstart', handleMousedown)
     }
   }
--- src/lib/Actions/zoomable.ts
+++ src/lib/Actions/zoomable.ts
@@ -1,17 +1,28 @@
+import { throttle } from '$lib/CGraph'
+
 type ZoomableParams = {
-  onZoom?: (delta: number, center: { x: number, y: number }, isTouch: boolean) => void
+  onZoom: (delta: number, center: { x: number, y: number }, isTouch: boolean) => void
 }
 
-export default function (node: HTMLElement, params: ZoomableParams): { destroy?: () => void } {
+export default function (node: HTMLElement | SVGElement, params?: ZoomableParams) {
   let dist: number
 
-  function scroll(e: WheelEvent) {
-    e.preventDefault()
-
-    if (params.onZoom) {
+  const trotZoom = throttle((e: WheelEvent | TouchEvent) => {
+    if (!params || !params.onZoom) return
+    if (e instanceof WheelEvent) {
+      e.preventDefault()
       params.onZoom(e.deltaY, { x: e.clientX, y: e.clientY }, false)
+    } else {
+      const currentDist = Math.hypot(
+        e.touches[0].pageX - e.touches[1].pageX,
+        e.touches[0].pageY - e.touches[1].pageY)
+      const delta = dist - currentDist
+      dist = currentDist
+      const centerX = (e.touches[0].pageX + e.touches[1].pageX) / 2
+      const centerY = (e.touches[0].pageY + e.touches[1].pageY) / 2
+      params.onZoom(delta, { x: centerX, y: centerY }, true)
     }
-  }
+  }, 50)
 
   function touchStart(e: TouchEvent) {
     e.preventDefault()
@@ -19,34 +30,26 @@ export default function (node: HTMLElement, params: ZoomableParams): { destroy?:
       dist = Math.hypot(
         e.touches[0].pageX - e.touches[1].pageX,
         e.touches[0].pageY - e.touches[1].pageY)
-      window.addEventListener('touchmove', handleTouchmove)
+      window.addEventListener('touchmove', trotZoom)
       window.addEventListener('touchend', handleTouchend)
     }
   }
 
-  function handleTouchmove(e: TouchEvent) {
-    const currentDist = Math.hypot(
-      e.touches[0].pageX - e.touches[1].pageX,
-      e.touches[0].pageY - e.touches[1].pageY)
-    const delta = dist - currentDist
-    dist = currentDist
-    if (params.onZoom) {
-      const centerX = (e.touches[0].pageX + e.touches[1].pageX) / 2
-      const centerY = (e.touches[0].pageY + e.touches[1].pageY) / 2
-      params.onZoom(delta, { x: centerX, y: centerY }, true)
-    }
-  }
   function handleTouchend() {
-    window.removeEventListener('touchmove', handleTouchmove)
+    window.removeEventListener('touchmove', trotZoom)
     window.removeEventListener('touchend', handleTouchend)
   }
 
-  node.addEventListener('wheel', scroll)
+  // @ts-ignore
+  node.addEventListener('wheel', trotZoom)
+  // @ts-ignore
   node.addEventListener('touchstart', touchStart)
 
   return {
     destroy() {
-      node.removeEventListener('wheel', scroll)
+      // @ts-ignore
+      node.removeEventListener('wheel', trotZoom)
+      // @ts-ignore
       node.removeEventListener('touchstart', touchStart)
     }
   }
