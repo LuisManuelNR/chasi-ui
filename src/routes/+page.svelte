aa9e9e1ba8b14d99208b8f198753b02f5ef9cd2b
Chasi <luismanuelchazy@gmail.com>
2024-04-05 12:21:43 +0200

    fix loading class on ghost buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+
+	let loading = false
+
+	onMount(() => {
+		setInterval(() => {
+			loading = !loading
+		}, 2000)
+	})
+</script>
+
 <h1>@chasi/ui</h1>
+
+<button class="btn brand" class:loading> hello </button>
+<button class="btn ghost accent-text" class:loading> hello </button>
