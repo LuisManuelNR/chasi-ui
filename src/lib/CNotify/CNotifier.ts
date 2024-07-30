f5005985f99033d5c57f0b4a7d892701d544cac4
Chasi <luismanuelchazy@gmail.com>
2024-07-30 13:16:45 +0200

    add on listener to CNotifier

--- src/lib/CNotify/CNotifier.ts
+++ src/lib/CNotify/CNotifier.ts
@@ -24,13 +24,16 @@ const DEFAULT_TIMEOUT = 4000
 function send({ title, text, html, timeout = DEFAULT_TIMEOUT, type, target = '.notifications-holder' }: CNotifierParamsObj & { type: string }) {
 	const container = document.querySelector(target)
 	if (!container) throw 'Missing target element for Display notifications'
+	const props: ListenerParams = {
+		title,
+		text,
+		html
+	}
 	const noty = new CNotify({
 		target: container,
 		props: {
-			title,
-			text,
+			...props,
 			type,
-			html,
 			visible: true
 		},
 		intro: true
@@ -47,6 +50,7 @@ function send({ title, text, html, timeout = DEFAULT_TIMEOUT, type, target = '.n
 			clearTimeout(timer)
 		}
 	})
+	window.dispatchEvent(new CustomEvent(`notifier-${type}`, { detail: props }))
 }
 
 function error(opt: CNotifierParams): void {
@@ -70,8 +74,22 @@ function parseOpts(opt: CNotifierParams): CNotifierParamsObj {
 	return opt
 }
 
+type ListenerParams = { title?: string, text?: string, html?: string }
+function onSend(type: 'error' | 'success' | 'info', callback: (data: ListenerParams) => void, opts?: AddEventListenerOptions | boolean) {
+	const trigger = (e: CustomEvent<ListenerParams>) => {
+		callback(e.detail)
+	}
+	//@ts-ignore
+	window.addEventListener(`notifier-${type}`, trigger, opts)
+	return () => {
+		//@ts-ignore
+		window.removeEventListener(`notifier-${type}`, trigger, opts)
+	}
+}
+
 export const CNotifier = {
 	error,
 	info,
-	success
+	success,
+	on: onSend
 }
