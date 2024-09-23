861474e7285108fc1d401e6f656b93977a4a3a09
Chazy <luismanuelchazy@gmail.com>
2024-09-23 12:24:42 +0200

    fix icon on safari

--- src/lib/CIcon/CIcon.svelte
+++ src/lib/CIcon/CIcon.svelte
@@ -5,7 +5,14 @@
 	export { klass as class }
 </script>
 
-<svg viewBox="0 0 24 24" aria-hidden="true" class="c-icon {klass}" width={size} height={size}>
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	viewBox="0 0 {size} {size}"
+	aria-hidden="true"
+	style:width="{size}px"
+	style:height="{size}px"
+	class="c-icon {klass}"
+>
 	<path d={icon} />
 </svg>
 
