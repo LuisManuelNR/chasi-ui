9fdd18fd616833040c5176671398e62d871a5215
Chazy <luismanuelchazy@gmail.com>
2025-03-16 21:50:43 +0100

    add debounce

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -87,3 +87,14 @@ export function normalizeText(str: string) {
 		.normalize('NFD')
 		.replace(/[\u0300-\u036F]/g, '')
 }
+
+export function debounce<F extends (...args: Parameters<F>) => ReturnType<F>>(
+	func: F,
+	waitFor: number
+): (...args: Parameters<F>) => void {
+	let timeout: number
+	return (...args: Parameters<F>): void => {
+		clearTimeout(timeout)
+		timeout = setTimeout(() => func(...args), waitFor)
+	}
+}
