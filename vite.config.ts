7be832e933ac33627234cc673114f7752df2a476
Chazy <luismanuelchazy@gmail.com>
2025-10-02 13:11:37 +0200

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,6 @@ export default defineConfig({
 	server: {
 		port: 5000
 	},
-	css: {
-		preprocessorOptions: {
-			scss: {
-				api: 'modern' // or "modern"
-			}
-		}
-	},
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
